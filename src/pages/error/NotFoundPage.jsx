2e5b4fe51fd5edcf94c5bce2eebf86ec0e832432
Ruslan Chyhryn <rusrus.0110@gmail.com>
2026-07-07 17:25:34 +0200

    Build not found page

--- src/pages/error/NotFoundPage.jsx
+++ src/pages/error/NotFoundPage.jsx
@@ -1,12 +1,39 @@
-import { Link } from "react-router-dom";
+import notFoundImage from "../../assets/auth-phone.png";
+
+import "../../styles/not_found.css";
 
 function NotFoundPage() {
   return (
-    <div className="not-found-page">
-      <h1>404</h1>
-      <p>Page not found</p>
-      <Link to="/feed">Back to home</Link>
-    </div>
+    <main className="not-found-page">
+      <section className="not-found-container">
+        <div className="not-found-image-wrap">
+          <img
+            src={notFoundImage}
+            alt="ICHgram mobile preview"
+            className="not-found-image"
+          />
+        </div>
+
+        <div className="not-found-content">
+          <div className="not-found-title">
+            Oops! Page Not Found (404 Error)
+          </div>
+
+          <div className="not-found-text">
+            <p>
+              We&apos;re sorry, but the page you&apos;re looking for
+              doesn&apos;t seem to exist.
+            </p>
+
+            <p>
+              If you typed the URL manually, please double-check the spelling.
+            </p>
+
+            <p>If you clicked on a link, it may be outdated or broken.</p>
+          </div>
+        </div>
+      </section>
+    </main>
   );
 }
 
